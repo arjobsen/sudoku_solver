52679ada9cfcef0339b4782350db4cc1c889a94e
Arjen <arjen@bos4.nl>
2022-05-24 20:19:28 +0200

    Algoritme 2 (invullen enige optie) toegevoegd. Bij de 3 sterren sudoku komt hij nu even ver als ik zelf met de hand!

---
+++ README.md
@@ -0,0 +1,4 @@
+# README.md
+
+## Todo's
+* Herhaal algoritme 1 (uitsluiten van de opties) na elke invulling van algoritme 2 (invullen enige optie)
--- sudoku_solver.py
+++ sudoku_solver.py
@@ -35,7 +35,7 @@ def print_sudoku():
                 # print(sudoku[row][col], end=' ')
         print('')
     
-    print(f'\nIngevuld: {progress}/81')
+    print(f'\nIngevuld: {progress}/81\n\n\n')
     return progress
 
 
@@ -62,7 +62,12 @@ while not solved:
         break
     i += 1
     
-    # Algoritme 1
+    # Dev break
+    if i > 10:
+        print('BREAK')
+        break
+    
+    # ALGORITME 1
     # Uitsluiten van de al ingevulde getallen
     
     # Rijen
@@ -124,6 +129,121 @@ while not solved:
     sleep(0.3)
     print_sudoku()
     
-    # Dev break
-    if i > 50:
-        break
+    
+    # ALGORITME 2
+    # Als in 1 rij, kolom of vak maar 1 mogelijke plaats is voor een bepaald getal, vul die dan in
+    
+    # Rijen
+    for row in range(9):
+        opties = dict()
+        ingevulds = set()
+        for col in range(9):
+            if type(sudoku[row][col]) != set:
+                # Voeg bij ingevulds
+                ingevulds.add(sudoku[row][col])
+                continue
+            
+            # Leg de opties vast
+            for v in sudoku[row][col]:
+                if v not in opties.keys():
+                    opties[v] = 1
+                else:
+                    opties[v] += 1
+            
+        # Nu loop nogmaals over de cellen en als het de enige optie is, vul die dan in
+        for k, v in opties.items():
+            if v == 1 and k not in ingevulds:
+                for col in range(9):
+                    if type(sudoku[row][col]) == set and k in sudoku[row][col]:
+                        sudoku[row][col] = k
+                        ingevulds.add(k)
+                        break
+                    
+    sudoku = invullen(sudoku)
+    sleep(0.3)
+    print_sudoku()
+    
+    if sudoku[1][8] == 5:
+        print("BREAK")
+    
+    
+    # Kolommen
+    for col in range(9):
+        opties = dict()
+        ingevulds = set()
+        for row in range(9):
+            if type(sudoku[row][col]) != set:
+                # Voeg bij ingevulds
+                ingevulds.add(sudoku[row][col])
+                continue
+            
+            # Leg de opties vast
+            for v in sudoku[row][col]:
+                if v not in opties.keys():
+                    opties[v] = 1
+                else:
+                    opties[v] += 1
+            
+        # Nu loop nogmaals over de cellen en als het de enige optie is, vul die dan in
+        for k, v in opties.items():
+            if v == 1 and k not in ingevulds:
+                for row in range(9):
+                    if type(sudoku[row][col]) == set and k in sudoku[row][col]:
+                        sudoku[row][col] = k
+                        ingevulds.add(k)
+                        break            
+
+    sudoku = invullen(sudoku)
+    sleep(0.3)
+    print_sudoku()
+    
+    if sudoku[1][8] == 5:
+        print("BREAK")
+    
+    
+    if i == 3:
+        print("break")
+    
+    # Vakken
+    vak_ranges = [range(3), range(3,6), range(6,9)]
+    for h_range in vak_ranges:
+        for v_range in vak_ranges:
+                    
+            ingevulds = set()
+            opties = dict()
+            for row in h_range:
+                for col in v_range:
+                    if type(sudoku[row][col]) == int:
+                        ingevulds.add(sudoku[row][col])
+                    
+                    if type(sudoku[row][col]) == set:
+                        # Leg de mogelijke opties van de cell vast
+                        for v in sudoku[row][col]:
+                            if v not in opties.keys():
+                                opties[v] = 1
+                            else:
+                                opties[v] += 1
+                        
+            # Nu loop nogmaals over de cellen en als het de enige optie is, vul die dan in
+            for k, v in opties.items():
+                if v == 1 and k not in ingevulds:
+                    for row in h_range:
+                        for col in v_range:
+                            if type(sudoku[row][col]) == set and k in sudoku[row][col]:
+                                sudoku[row][col] = k
+                                ingevulds.add(k)
+                                
+                                if sudoku[1][8] == 5:
+                                    print("BREAK")
+                                    
+                                break  
+
+    
+    sudoku = invullen(sudoku)
+    sleep(0.3)
+    print_sudoku()
+    
+    if sudoku[1][8] == 5:
+        print("BREAK")
+    
+
